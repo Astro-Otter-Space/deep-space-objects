8ec9a9d6cc54006394ca8e3fe3ef03932212f64d
Stephane Meaudre <balistik.fonfon@gmail.com>
2018-12-07 16:44:21 +0100

    Test Vie.js components

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,10 @@
 var Encore = require('@symfony/webpack-encore');
 
 Encore
-    // directory where compiled assets will be stored
     .setOutputPath('public/build/')
-    // public path used by the web server to access the output path
     .setPublicPath('/build')
-    // only needed for CDN's or sub-directory deploy
-    //.setManifestKeyPrefix('build/')
-
+    .cleanupOutputBeforeBuild()
+    .enableSourceMaps(!Encore.isProduction())
     /*
      * ENTRY CONFIG
      *
@@ -17,14 +14,14 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
-    .addEntry('app', './assets/js/main.js')
-
+    .addEntry('app', './assets/js/app.js')
+    .addEntry('search', './assets/js/')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
-    .enableSingleRuntimeChunk()
+    // .enableSingleRuntimeChunk()
 
     /*
      * FEATURE CONFIG
@@ -33,11 +30,11 @@ Encore
      * list of features, see:
      * https://symfony.com/doc/current/frontend.html#adding-more-features
      */
-    .cleanupOutputBeforeBuild()
-    .enableBuildNotifications()
-    .enableSourceMaps(!Encore.isProduction())
+
+    // .enableBuildNotifications()
+
     // enables hashed filenames (e.g. app.abc123.css)
-    .enableVersioning(Encore.isProduction())
+
 
     // enables Sass/SCSS support
     //.enableSassLoader()
