f4db51ad068631e9dea889d0e91a98bed9b087d1
Stephane Meaudre <balistik.fonfon@gmail.com>
2018-12-07 16:13:47 +0100

    Add app.js

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,8 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
-    .addEntry('app', './assets/js/app.js')
+    .addEntry('app', './assets/js/main.js')
+
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
@@ -46,6 +47,7 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
+    .enableVueLoader();
 ;
 
 module.exports = Encore.getWebpackConfig();
