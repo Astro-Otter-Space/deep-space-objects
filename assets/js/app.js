eef72d4b69830364d55b29a5a893b3c830bb946e
Stephane Meaudre <smeaudre@kaliop.com>
2018-12-10 17:39:00 +0100

    Test vue.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,10 @@
 // https://www.cloudways.com/blog/symfony-vuejs-app/
 // https://fr.vuejs.org/v2/examples/select2.html
-import Vue from 'vue';
+import Vue from 'vue'
 
-import App from './components/App'
+import switchLang from './components/SwitchLang'
 
 new Vue({
-  el: '#app',
-  components: {App}
-});
+  el: '#switch-lang',
+  components: {switchLang}
+}).$mount('#switchLang');
---
+++ assets/js/components/SwitchLang.vue
@@ -0,0 +1,15 @@
+<template>
+  <select>
+    <option v-for="option in dataLangs" ></option>
+  </select>
+</template>
+
+<script>
+  export default {
+    name: "select-lang",
+    props: ["dataLangs"],
+    data: function () {
+      return {}
+    }
+  }
+</script>
