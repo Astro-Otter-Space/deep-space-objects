dd2d8c286e4adedd5ffcb63cbfc1e31d2c5085d0
Stephane Meaudre <smeaudre@kaliop.com>
2018-12-12 17:20:05 +0100

    Fix assets

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,11 @@ var Encore = require('@symfony/webpack-encore');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 Encore
+    // directory where compiled assets will be stored
     .setOutputPath('public/build/')
+    // public path used by the web server to access the output path
     .setPublicPath('/build')
+
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
     /*
