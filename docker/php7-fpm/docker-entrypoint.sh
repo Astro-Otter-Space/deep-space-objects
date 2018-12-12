8d5d074a84d3b5bfbe4b21774abefdea8dc5d5d1
Stephane Meaudre <smeaudre@kaliop.com>
2018-12-12 18:04:46 +0100

    Refactorinf docker stack - container PHP-FPM

---
+++ docker/php7-fpm/docker-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+groupadd -g $SITE_GID site
+useradd --uid $SITE_UID --gid $SITE_GID --groups $SITE_GID -s /bin/bash site -d /home/site
+mkdir /home/site/.config
+chown -R site:site /home/site
+
+php-fpm -F
