b5b49e56201794b34e877898b3e51e410337b970
Stephane Meaudre <smeaudre@kaliop.com>
2018-12-13 15:41:12 +0100

    Some fix for docker

--- docker/php7-fpm/docker-entrypoint.sh
+++ docker/php7-fpm/docker-entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 groupadd -g $SITE_GID site
-useradd --uid $SITE_UID --gid $SITE_GID --groups $SITE_GID -s /bin/bash site -d /home/site
-
+useradd -u $SITE_UID -g $SITE_GID -G $SITE_GID -s /bin/bash site -d /home/site
 mkdir /home/site/.config
 chown -R site:site /home/site
 
