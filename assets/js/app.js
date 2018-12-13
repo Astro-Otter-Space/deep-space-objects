ebdcc48984d237007972d76e72b027fe4c1dc68c
Stephane Meaudre <smeaudre@kaliop.com>
2018-12-13 17:07:35 +0100

    Fix docker + ass data + fix JS config

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,17 @@
 // https://www.cloudways.com/blog/symfony-vuejs-app/
 // https://fr.vuejs.org/v2/examples/select2.html
 import '../css/app.scss';
-
-import Vue from 'vue'
-
-import switchLang from './components/SwitchLang'
-
-const dataLangs = [];
-
-new Vue({
-  el: '#switch-lang',
-  components: {switchLang},
-  data: {
-    dataLangs
-  }
-}).$mount('#switchLang');
+//
+// import Vue from 'vue'
+//
+// import switchLang from './components/SwitchLang'
+//
+// const dataLangs = [];
+//
+// new Vue({
+//   el: '#switch-lang',
+//   components: {switchLang},
+//   data: {
+//     dataLangs
+//   }
+// }).$mount('#switchLang');
--- webpack.config.js
+++ webpack.config.js
@@ -24,15 +24,11 @@ Encore
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
-    // .enableSingleRuntimeChunk()
-
-    .enableBuildNotifications()
+    .enableSingleRuntimeChunk()
+    // .enableBuildNotifications()
 
     // enables Sass/SCSS support
     .enableSassLoader()
-
-    // uncomment if you use TypeScript
-    //.enableTypeScriptLoader()
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
