d5436d1fceef496633b8ef3e43b560e445b371ca
Stephane Meaudre <smeaudre@kaliop.com>
2018-12-20 15:20:07 +0100

    Fix Slider homepage

--- assets/js/Widgets/Homepage/App.vue
+++ assets/js/Widgets/Homepage/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
+    <h1 class="AppSlider__title">{{title}}</h1>
     <slider
       :images="[
-        'https://raw.githubusercontent.com/HamHamFonFon/deep-space-objects/master/assets/images/background/bg-1.jpg',
-        'https://raw.githubusercontent.com/HamHamFonFon/deep-space-objects/master/assets/images/background/bg-2.jpg',
-        'https://raw.githubusercontent.com/HamHamFonFon/deep-space-objects/master/assets/images/background/bg-3.jpg',
-        'https://raw.githubusercontent.com/HamHamFonFon/deep-space-objects/master/assets/images/background/bg-4.jpg'
+        '/assets/images/build/images/background/bg-1.jpg',
+        '/assets/images/build/images/background/bg-2.jpg',
+        '/assets/images/build/images/background/bg-3.jpg',
+        '/assets/images/build/images/background/bg-4.jpg'
       ]"
+      :sub-title="${subTitle}"
     />
-    <h2>{{subTitle}}</h2>
+    <SearchAutocomplete/>
+    <h2 class="AppSlider__subTitle">{{subTitle}}</h2>
   </div>
 </template>
 
 <script>
   import Slider from './components/Slider'
-  // import SearchAutocomplete from '../Homepage/components/SearchAutocomplete'
+  import SearchAutocomplete from "./components/SearchAutocomplete";
 
   let homeTitle = document.querySelector('div[data-home-widget]').dataset.homeTitle;
 
   export default {
     name: "App",
-    components: {Slider},
-    props: {
-      subTitle: homeTitle
+    components: {
+      SearchAutocomplete,
+      Slider
+    },
+    data() {
+      return {
+        subTitle: homeTitle
+      }
     }
   }
 </script>
--- assets/js/Widgets/Homepage/components/Slider.vue
+++ assets/js/Widgets/Homepage/components/Slider.vue
@@ -1,26 +1,35 @@
 <template>
   <div class="AppSlider">
     <transition-group
+      v-touch:swipe.right="next"
+      v-touch:swipe.left="prev"
       :duration="1000"
       :style="{ paddingBottom: `${aspectRatio}%` }"
       tag="div"
-      v-touch:swipe.right="next"
-      v-touch:swipe.left="prev"
       enter-active-class="AppSlider__enterActive"
       enter-class="AppSlider__enter"
       leave-active-class="AppSlider__leaveActive"
       leave-to-class="AppSlider__leaveTo"
       class="AppSlider__slides"
     >
-      <img
+      <!--<img-->
+        <!--v-for="(image, index) in images"-->
+        <!--v-show="activeIndex !== index"-->
+        <!--:key="index"-->
+        <!--class="AppSlider__image"-->
+        <!--:src="image"-->
+        <!--:alt="subTitle"-->
+      <!--/>-->
+      <div
         v-for="(image, index) in images"
-        v-show="activeIndex === index"
         :key="index"
-        :src="image"
-        class="AppSlider__image"
-        alt=""
-      />
+        :style="{ backgroundImage: `url(${image})` }"
+        v-show="activeIndex === index"
+        class="AppSlider__dImage"
+      >
+      </div>
     </transition-group>
+    <h2 class="AppSlider__subTitle">{{ subTitle }}</h2>
   </div>
 </template>
 
@@ -28,29 +37,33 @@
   export default {
     name: "Slider",
     props: {
-      titre: "Deep Space Objects",
       height: {
-        default: 1080,
-        type: Number
-      },
-      interval: {
-        default: 10000,
-        type: Number
-      },
-      width: {
         default: 1920,
         type: Number
       },
       images: {
         default: () => [],
         type: Array
+      },
+      interval: {
+        default: 10000,
+        type: Number
+      },
+      width: {
+        default: 3072,
+        type: Number
+      },
+      subTitle: {
+        default: '',
+        type: String
       }
     },
     data() {
       return {
         activeIndex: 0,
         paused: false,
-        time: this.interval
+        time: this.interval,
+        subTitle: "Discover beauties and mysteries of the universe"
       };
     },
     computed: {
@@ -84,10 +97,10 @@
         const precision = 100;
         const clock = setInterval(() => {
           if (!this.paused) this.time -= precision;
-          if (0 >= this.time) this.next();
+          if (this.time <= 0) this.next();
         }, precision);
 
-        this.$once("hook:destroyed", () => clearInterval(clock));
+        this.$once(`hook:destroyed`, () => clearInterval(clock));
       }
     }
   };
