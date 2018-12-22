1cf6a38f109dda1cc8a5373eb23e060f63e4b9da
HamHamFonFon <balistik.fonfon@gmail.com>
2018-12-22 16:33:44 +0100

    Fix templates, scss, layout and data

---
+++ src/Command/ConvertSrcToBulkCommand.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+/**
+ * Class ConvertSrcToBulkCommand
+ * @package App\Command
+ */
+class ConvertSrcToBulkCommand extends Command
+{
+    /** @var KernelInterface */
+    private $kernel;
+
+    protected static $defaultName = "dso:convert-bulk";
+
+    protected static $listType = ['dso20', 'constellations', 'constellations'];
+
+    const PATH_SOURCE = '/config/elasticsearch/sources/';
+    const BULK_SOURCE = '/config/elasticsearch/bulk/';
+
+
+    /**
+     * ConvertSrcToBulkCommand constructor.
+     * @param KernelInterface $kernel
+     */
+    public function __construct(KernelInterface $kernel)
+    {
+        $this->kernel = $kernel->getProjectDir();
+        parent::__construct();
+    }
+
+    /**
+     *
+     */
+    protected function configure()
+    {
+        $this
+            ->setDescription('Convert source file into bulk for Elastic Search')
+            ->addArgument('type', InputArgument::REQUIRED, 'List of values : ' . implode(', ', self::$listType));
+        ;
+    }
+
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int|null|void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if ($input->hasArgument('type') && in_array($input->getArgument('type'), self::$listType)) {
+
+            $type = $input->getArgument('type');
+
+            $inputFilename = sprintf('%s.src.json', $type);
+            $inputFile = $this->kernel . self::PATH_SOURCE . $inputFilename;
+
+            if (file_exists($inputFile) && is_readable($inputFile)) {
+                $data = $this->openFile($inputFile);
+                dump(json_last_error_msg());
+            } else {
+                $output->writeln(sprintf("File %s not exist or not readable", $inputFile));
+            }
+
+        } else {
+            $output->writeln(sprintf("Argument %s not available", $input->getArgument('type')));
+        }
+    }
+
+    /**
+     * Open file and convert into array
+     * @param $file
+     * @return mixed
+     */
+    private function openFile($file) {
+        return json_decode(file_get_contents($file), true);
+    }
+
+    private function sanitizeData($data)
+    {
+
+
+
+    }
+}
---
+++ src/Controller/LayoutController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class LayoutController
+ * @package App\Controller
+ */
+class LayoutController extends AbstractController
+{
+    const HTTP_TTL = 31556952;
+
+
+    /**
+     * Header
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function header(Request $request)
+    {
+        $result = [];
+
+        /** @var Response $response */
+        $response = new Response();
+        $response->setSharedMaxAge(0);
+        return $this->render('includes/layout/header.html.twig', $result, $response);
+    }
+
+    /**
+     * Footer
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function footer(Request $request)
+    {
+        $result = [];
+
+        /** @var Response $response */
+        $response = new Response();
+        $response->setSharedMaxAge(0);
+
+        return $this->render('includes/layout/footer.html.twig', $result, $response);
+    }
+
+}
