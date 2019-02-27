79ad0a8db5c79d5132ba4749aebade0e07dd2db1
HamHamFonFon <balistik.fonfon@gmail.com>
2019-02-27 22:53:24 +0100

    Command check astrobinId

--- src/Command/CheckAstrobinImageCommand.php
+++ src/Command/CheckAstrobinImageCommand.php
@@ -5,6 +5,9 @@ namespace App\Command;
 
 
 use App\Repository\DsoRepository;
+use Astrobin\Exceptions\WsResponseException;
+use Astrobin\Response\Image;
+use Astrobin\Services\GetImage;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -29,8 +32,7 @@ class CheckAstrobinImageCommand extends Command
     public function __construct(DsoRepository $dsoRepository)
     {
         $this->dsoRepository = $dsoRepository;
-
-        parent::_construct();
+        parent::__construct();
     }
 
     /**
@@ -47,11 +49,28 @@ class CheckAstrobinImageCommand extends Command
      * @param OutputInterface $output
      *
      * @return int|void|null
+     * @throws \Astrobin\Exceptions\WsException
+     * @throws \ReflectionException
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        dump(__CLASS__);
+        $failedAstrobinId = [];
         $listAstrobinId = $this->dsoRepository->getAstrobinId();
-        dump($listAstrobinId);
+        if (0 < count($listAstrobinId)) {
+            foreach ($listAstrobinId as $astrobinId) {
+                try {
+                    /** @var GetImage $image */
+                    $image = new GetImage();
+
+                    $result = $image->getImageById($astrobinId);
+                } catch (WsResponseException $e) {
+                    $failedAstrobinId[] = $astrobinId;
+                    continue;
+                }
+            }
+        }
+        $output->write(implode(' - ', $failedAstrobinId));
     }
 
 
