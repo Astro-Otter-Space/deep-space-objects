b40c0a5935ad5a7b4c391c6ca079d805f8765071
HamHamFonFon <balistik.fonfon@gmail.com>
2019-04-27 19:58:47 +0200

    List observations

--- assets/js/Widgets/Observation/List.vue
+++ assets/js/Widgets/Observation/List.vue
@@ -1,13 +1,43 @@
 <template>
-  
+  <div>
+    <section class="Dso__main">
+      <div class="Dso__container Dso__noHeader">
+
+        <h2 class="Dso__title">Observations list</h2>
+
+        <searchautocomplete
+          ref="observationsearch"
+          :searchPlaceholder="Test"
+          :customClasses="autoCompleteClasse"
+          :url="urlSearch"
+        />
+      </div>
+
+      <h3 class="Dso__title">Location des observations</h3>
+    </section>
+  </div>
 </template>
 
 <script>
+  import Searchautocomplete from "./../Homepage/components/Searchautocomplete"
+
+  let urlSearchObs = document.querySelector('div[data-observations-list]').dataset.searchRoute;
+
   export default {
-    name: "List"
+    name: "App",
+    components: {
+      Searchautocomplete
+    },
+    data () {
+      return {
+        urlSearch: urlSearchObs,
+        autoCompleteClasse: {
+          wrapper: 'AppHeader__wrapper',
+          input: 'AppSearch__inputText',
+          list: 'AppHeader__list'
+        },
+      }
+    }
+
   }
 </script>
-
-<style scoped>
-
-</style>
