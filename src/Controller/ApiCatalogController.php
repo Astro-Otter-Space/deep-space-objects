0f91c29e46b723fd35b90d64c825f22f310efdae
Stephane Meaudre <smeaudre@kaliop.com>
2019-07-02 17:40:18 +0200

    Update API test conf

--- src/Controller/ApiCatalogController.php
+++ src/Controller/ApiCatalogController.php
@@ -1,11 +1,10 @@
 <?php
 
-namespace App\Api\Controller;
+namespace App\Controller;
 
 use App\Entity\Dso;
 use App\Managers\DsoManager;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
-use FOS\RestBundle\View\View;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -15,7 +14,7 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
  *
  * @package App\Api\Controller
  */
-final class CatalogController extends AbstractFOSRestController
+final class ApiCatalogController extends AbstractFOSRestController
 {
     /** @var DsoManager  */
     private $dsoManager;
@@ -43,7 +42,6 @@ final class CatalogController extends AbstractFOSRestController
     {
         /** @var Dso $dso */
         $dso = $this->dsoManager->buildDso($dsoId);
-        dump($dso);
         if (!$dso instanceof Dso) {
             throw new NotFoundHttpException();
         }
