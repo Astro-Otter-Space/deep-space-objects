831c5945cabba8534d1fe530f759b3f1c218f363
Stephane Meaudre <smeaudre@kaliop.com>
2019-07-02 13:39:57 +0200

    Fix conf API Rest

--- src/Api/Controller/CatalogController.php
+++ src/Api/Controller/CatalogController.php
@@ -8,11 +8,14 @@ use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\View\View;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * Class DsoController
  *
  * @package App\Api\Controller
+ * @Route("/api", name="api_")
  */
 final class CatalogController extends AbstractFOSRestController
 {
@@ -35,14 +38,15 @@ final class CatalogController extends AbstractFOSRestController
      * @return View
      * @throws \Astrobin\Exceptions\WsException
      * @throws \ReflectionException
+     *
      * @Rest\Get("/item/{$id}")
      */
-    public function getItem(string $id): View
+    public function getItem(string $id)
     {
         /** @var Dso $dso */
         $dso = $this->dsoManager->buildDso($id);
         if (!$dso instanceof Dso) {
-            throw new NotFoundException();
+            throw new NotFoundHttpException();
         }
 
         return View::create($id, Response::HTTP_OK);
