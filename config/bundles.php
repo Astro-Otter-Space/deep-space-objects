3bbb3f06e42e7793fff13c809ec4d9ab8ab4fbd5
Stephane Meaudre <smeaudre@kaliop.com>
2019-08-02 15:10:16 +0200

    Install LexikJWTBundle

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
