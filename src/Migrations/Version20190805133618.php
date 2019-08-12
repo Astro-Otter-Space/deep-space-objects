a462d95b74c760d2daf302e3638b7b63c9785469
Stephane Meaudre <smeaudre@kaliop.com>
2019-08-12 09:01:04 +0200

    Fix minoir migrations

--- src/Migrations/Version20190805133618.php
+++ src/Migrations/Version20190805133618.php
@@ -22,7 +22,7 @@ final class Version20190805133618 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE api_users (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(25) NOT NULL COLLATE utf8mb4_unicode_ci, password VARCHAR(64) NOT NULL COLLATE utf8mb4_unicode_ci, is_active TINYINT(1) NOT NULL, email VARCHAR(60) NOT NULL COLLATE utf8mb4_unicode_ci, UNIQUE INDEX UNIQ_3016F139E7927C74 (email), UNIQUE INDEX UNIQ_3016F139F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('CREATE TABLE IF NOT EXISTS api_users (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(25) NOT NULL COLLATE utf8mb4_unicode_ci, password VARCHAR(64) NOT NULL COLLATE utf8mb4_unicode_ci, is_active TINYINT(1) NOT NULL, email VARCHAR(60) NOT NULL COLLATE utf8mb4_unicode_ci, UNIQUE INDEX UNIQ_3016F139E7927C74 (email), UNIQUE INDEX UNIQ_3016F139F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB COMMENT = \'\' ');
     }
 
     public function down(Schema $schema) : void
--- src/Migrations/Version20190810170943.php
+++ src/Migrations/Version20190810170943.php
@@ -22,7 +22,6 @@ final class Version20190810170943 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE foo (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8C736521E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('DROP INDEX UNIQ_3016F139F85E0677 ON api_users');
         $this->addSql('ALTER TABLE api_users ADD roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', DROP username');
     }
@@ -32,7 +31,6 @@ final class Version20190810170943 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE foo');
         $this->addSql('ALTER TABLE api_users ADD username VARCHAR(25) NOT NULL COLLATE utf8mb4_unicode_ci, DROP roles');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_3016F139F85E0677 ON api_users (username)');
     }
