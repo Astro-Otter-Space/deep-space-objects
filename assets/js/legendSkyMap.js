078b678a514c4f97092416ef07ca8c77331f67fe
HamHamFonFon <balistik.fonfon@gmail.com>
2019-08-22 23:58:31 +0200

    Change color code

--- assets/js/legendSkyMap.js
+++ assets/js/legendSkyMap.js
@@ -1,24 +1,33 @@
+let COLOR_CLUST_GAL = '#8B1A1A';
+let COLOR_GAL_G = '#C62828';
+let COLOR_GAL_S = '#D32F2F';
+let COLOR_GAL_SO = '#E53935';
+let COLOR_GAL_SD = '#F44336';
+let COLOR_GAL_E = '#EF5350';
+let COLOR_GAL_I = '#E57373';
 let COLOR_GALAXY = '#ff0000'; // Galaxies
-let COLOR_OC = '#ffcc00'; // Open cluster
+
+let COLOR_OC = '#FF4626'; // Open cluster
 let COLOR_GC = '#ff9900'; // Global Cluster
-let COLOR_EN = '#FF4626'; // Emission nebula
-let COLOR_RN = '#10ff00'; // Reflection nebula
-let COLOR_PN = '#1eccbf'; // Planetary nebula
+
+let COLOR_SFR = '#EEFF41'; // Star forming nebula
+let COLOR_EN = '#64DD17'; // Emission nebula
+let COLOR_RN = '#00E5FF'; // Reflection nebula
+let COLOR_PN = '#00BFA5'; // Planetary nebula
+let COLOR_BN = '#1E88E5'; //BRIGHT NEBULA
 let COLOR_SNR = '#1838CC'; // Supernova remnant
-let COLOR_SFR = '#cc00ff'; // Star forming nebula
-let COLOR_BN = '#ff00cc'; //BRIGHT NEBULA
-let COLOR_DN = '#4b42f4';
+let COLOR_DN = '#5E35B1';
 let COLOR_POS = '#cccccc';
 let DEFAULT = '#ffffff';
 
 export const color = {
-  gg: COLOR_GALAXY,
-  g: COLOR_GALAXY,
-  s: COLOR_GALAXY,
-  s0: COLOR_GALAXY,
-  sd: COLOR_GALAXY,
-  e: COLOR_GALAXY,
-  i: COLOR_GALAXY,
+  gg: COLOR_CLUST_GAL,
+  g: COLOR_GAL_G, // generic
+  s: COLOR_GAL_S, // spirale
+  s0: COLOR_GAL_SO, // lenticulaire
+  sd: COLOR_GAL_SD, // naine
+  e: COLOR_GAL_E, // eliptique
+  i: COLOR_GAL_I, // irreguliere
   oc: COLOR_OC,
   gc: COLOR_GC,
   en: COLOR_EN,
