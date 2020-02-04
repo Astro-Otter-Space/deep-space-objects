576701e312def66d6dd8f19565780c8b59604e44
HamHamFonFon <balistik.fonfon@gmail.com>
2020-02-04 19:41:04 +0100

    HTTP cache invalidate method

--- src/CacheKernel.php
+++ src/CacheKernel.php
@@ -3,6 +3,8 @@
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Class CacheKernel
@@ -19,5 +21,34 @@ class CacheKernel extends HttpCache
         return [
             'default_ttl' => 31556952
         ];
+    }
+
+    /**
+     * @param Request $request
+     * @param bool $catch
+     *
+     * @return Response
+     * @throws \Exception
+     */
+    protected function invalidate(Request $request, $catch = false): Response
+    {
+        if ('PURGE' !== $request->getMethod()) {
+            return parent::invalidate($request, $catch);
+        }
+
+        if ('127.0.0.1' !== $request->getClientIp()) {
+            return new Response('Invalid HTTP cache', Response::HTTP_BAD_REQUEST);
+        }
+
+        /** @var Response $response */
+        $response = new Response();
+
+        if ($this->getStore()->purge($request->getUri())) {
+            $response->setStatusCode(Response::HTTP_OK, 'Purged');
+        } else {
+            $response->setStatusCode(Response::HTTP_NOT_FOUND, 'Not found');
+        }
+
+        return $response;
     }
 }
