ec27ef83594da37aa6a0b909d025afe809da2e1f
HamHamFonFon <balistik.fonfon@gmail.com>
2020-02-23 14:40:43 +0100

    Remove credit astrobin if default image

--- src/DataTransformer/DsoDataTransformer.php
+++ src/DataTransformer/DsoDataTransformer.php
@@ -87,7 +87,7 @@ final class DsoDataTransformer extends AbstractDataTransformer
             'discoverYear' => $entity->getDiscoverYear(),
             'ra' => $entity->getRa(),
             'dec' => $entity->getDec(),
-            'astrobin.credit' => (!is_null($entity->getImage()->title)) ? sprintf('"%s" %s %s', $entity->getImage()->title, Dso::DATA_CONCAT_GLUE, $entity->getImage()->user ) : ''
+            'astrobin.credit' => (!is_null($entity->getAstrobinId())) ? sprintf('"%s" %s %s', $entity->getImage()->title, Dso::DATA_CONCAT_GLUE, $entity->getImage()->user ) : ''
         ];
 
         return array_filter($data, function($value) {
