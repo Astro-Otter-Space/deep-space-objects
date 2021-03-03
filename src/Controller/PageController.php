72e5752af27f0397fec6ff1fc578c5c2e7d1b1d4
Stephane Meaudre <smeaudre@kaliop.com>
2021-03-03 16:39:44 +0100

    test

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -407,7 +407,7 @@ class PageController extends AbstractController
      */
     public function memcache(): Response
     {
-        echo $this->getParameter('%env(MEMCACHED_URL)%');
+        echo $this->getParameter('memcachedUrl');
         $memcached = new MemcachedAdapter('app.cache.dso');
         $m42 = $memcached->getItem(md5('m42'));
 
