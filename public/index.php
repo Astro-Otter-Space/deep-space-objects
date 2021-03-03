66f24c09bbe7746ee781967f1c8cc5100c790520
Stephane Meaudre <smeaudre@kaliop.com>
2021-03-03 17:06:18 +0100

    test index.php

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 //require dirname(__DIR__).'/config/bootstrap.php';
 require dirname(__DIR__).'/vendor/autoload.php';
 
-(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+(new Dotenv())->load(dirname(__DIR__).'/.env');
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
