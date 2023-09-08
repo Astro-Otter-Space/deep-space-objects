e0cd590a52c032cc4e723c1f739b3f42a6feaade
Stephane MEAUDRE <smeaudre@kaliop.com>
2023-09-08 09:33:24 +0200

    Add annotations API Rest Get dso

--- src/ControllerApi/Dso/Show.php
+++ src/ControllerApi/Dso/Show.php
@@ -11,6 +11,7 @@ use Symfony\Component\Serializer\Encoder\XmlEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\View\View;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 
