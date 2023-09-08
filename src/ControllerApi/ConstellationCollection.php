3ab47c63b9f86e2eb09dec418e9640589b3db1c0
Stephane MEAUDRE <smeaudre@kaliop.com>
2023-09-08 13:29:25 +0200

    Add API route GET ITEM constellation

---
+++ src/ControllerApi/ConstellationCollection.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\ControllerApi;
+
+use App\Managers\ConstellationManager;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Exception\InvalidParameterException;
+use FOS\RestBundle\Request\ParamFetcher;
+use FOS\RestBundle\Request\ParamFetcherInterface;
+use FOS\RestBundle\View\View;
+use FOS\RestBundle\Controller\Annotations\RequestParam;
+use FOS\RestBundle\Controller\Annotations\QueryParam;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+class ConstellationCollection extends AbstractFOSRestController
+{
+
+    public function __construct(private ConstellationManager $constellationManager) {}
+
+    /**
+     * @Route("/constellation/list", name="api_get_constellation_collection", methods={"GET"})
+     * @return View
+     * @throws \ReflectionException
+     * @throws \JsonException
+     */
+    public function getConstellationsCollection(): View
+    {
+        try {
+            $listConstellations = $this->constellationManager->getAllConstellations();
+        } catch (\Exception $e) {
+            throw new NotFoundHttpException('Constellations list not found');
+        }
+
+        $encoders = [new JsonEncoder()];
+        $normalizers = [new ObjectNormalizer()];
+
+        $serializer = new Serializer($normalizers, $encoders);
+        $formatedData = $serializer->normalize($listConstellations->getIterator()->getArrayCopy());
+
+        $view = View::create();
+        $view->setData($formatedData);
+        $view->setFormat('json');
+        return $view;
+    }
+}
