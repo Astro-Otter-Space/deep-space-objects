41be32063514eacba5477aa3a8092e1594af2f08
Stephane MEAUDRE <smeaudre@kaliop.com>
2023-09-08 11:05:29 +0200

    Change load DotEnv

--- public/index.php
+++ public/index.php
@@ -8,7 +8,9 @@ use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+if (file_exists(dirname(__DIR__).'/.env')) {
+    (new Dotenv())->loadEnv(dirname(__DIR__).'/.env');
+}
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
