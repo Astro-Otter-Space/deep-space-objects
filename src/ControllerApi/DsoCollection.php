4bc32d0c44576ff3dc3d4626c4fb46167e198a26
Stephane MEAUDRE <smeaudre@kaliop.com>
2023-09-08 11:51:41 +0200

    CHange annotation route

--- src/ControllerApi/DsoCollection.php
+++ src/ControllerApi/DsoCollection.php
@@ -6,11 +6,11 @@ use App\Classes\Utils;
 use App\Managers\DsoManager;
 use App\Repository\DsoRepository;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
-use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Exception\InvalidParameterException;
 use FOS\RestBundle\Request\ParamFetcher;
 use FOS\RestBundle\Request\ParamFetcherInterface;
 use FOS\RestBundle\View\View;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
@@ -26,7 +26,7 @@ class DsoCollection extends AbstractFOSRestController
     ) { }
 
     /**
-     * @Rest\Get("/dso/list", name="api_get_dso_collection")
+     * @Route("/dso/list", name="api_get_dso_collection", methods={"GET"})
      *
      * @Rest\QueryParam(name="constellation", requirements="\w+", default="")
      * @Rest\QueryParam(name="catalog", requirements="\w+", default="")
--- src/ControllerApi/DsoItem.php
+++ src/ControllerApi/DsoItem.php
@@ -6,11 +6,11 @@ use App\Controller\ControllerTraits\DsoTrait;
 use App\Managers\DsoManager;
 use App\Service\InjectionTrait\SymfonyServicesTrait;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
-use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Exception\InvalidParameterException;
 use FOS\RestBundle\Request\ParamFetcher;
 use FOS\RestBundle\Request\ParamFetcherInterface;
 use FOS\RestBundle\View\View;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
@@ -29,7 +29,7 @@ class DsoItem extends AbstractFOSRestController
     ) { }
 
     /**
-     * @Rest\Get("/dso/item/{id}", name="api_get_dso_item")
+     * @Route("/dso/item/{id}", name="api_get_dso_item", methods={"GET"})
      *
      * @param string $id
      * @return View
