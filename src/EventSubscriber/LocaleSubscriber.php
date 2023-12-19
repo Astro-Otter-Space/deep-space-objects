a200b0b7e15a1f01bb8196ce70358676cd87b096
Stephane MEAUDRE <smeaudre@kaliop.com>
2023-12-19 15:47:48 +0100

    dump locale

--- src/EventSubscriber/LocaleSubscriber.php
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -38,6 +38,7 @@ class LocaleSubscriber implements EventSubscriberInterface
             return;
         }
 
+var_dump($request->attributes->get('_locale'), $request->query->get('_locale'));
         // try to see if the locale has been set as a _locale routing parameter
         if ($locale = $request->attributes->get('_locale')) {
             $request->getSession()->set('_locale', $locale);
