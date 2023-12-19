4bf68ac59d694e0c006fba166b5c7f7e4d8f6641
Stephane MEAUDRE <smeaudre@kaliop.com>
2023-12-19 15:11:11 +0100

    Test iterator count

--- src/ControllerApi/Search.php
+++ src/ControllerApi/Search.php
@@ -49,16 +49,16 @@ class Search extends AbstractFOSRestController
         } catch (\Exception $e) {
             throw new WsException($e->getMessage());
         } finally {
-            $listDso = $serializer->normalize($listDso, null, ['groups' => 'search']);
+            $listDso = (0 < iterator_count($listDso)) ? $serializer->normalize($listDso, null, ['groups' => 'search']) : [];
         }
 
         try {
             $listConstellation = $constellationManager->searchConstellationsByTerms($searchTerm);
-            var_dump(iterator_count($listConstellation));
+            var_dump();
         } catch (\Exception $e) {
             throw new WsException($e->getMessage());
         } finally {
-            $listConstellation = $serializer->normalize($listConstellation, null, ['groups' => 'search']);
+            $listConstellation = (0 < iterator_count($listConstellation)) ? $serializer->normalize($listConstellation, null, ['groups' => 'search']) : [];
         }
 
         $formatedData = ['dsos' => $listDso, 'constellations' => $listConstellation];
