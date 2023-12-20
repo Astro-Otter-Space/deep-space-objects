63a7d47acc96c34f87ae0ec674dfb5b15f85eb38
Stephane MEAUDRE <smeaudre@kaliop.com>
2023-12-20 09:27:00 +0100

    Test locale as query parameter in api routes

--- src/EventSubscriber/LocaleSubscriber.php
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -37,14 +37,22 @@ class LocaleSubscriber implements EventSubscriberInterface
         if (!$request->hasPreviousSession()) {
             return;
         }
-
-
-        // try to see if the locale has been set as a _locale routing parameter
-        if ($locale = $request->attributes->get('_locale')) {
-            $request->getSession()->set('_locale', $locale);
+        
+        $path = $request->getPathInfo();
+        $pathParts = explode('/', trim($path, '/'));
+        
+        if (in_array('api', $pathParts, true)) {
+            if ($locale = $request->query->get('_locale')) {
+                $request->getSession()->set('_locale', $locale);
+            }
         } else {
-            // if no explicit locale has been set on this request, use one from the session
-	    $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
+            // try to see if the locale has been set as a _locale routing parameter
+            if ($locale = $request->attributes->get('_locale')) {
+                $request->getSession()->set('_locale', $locale);
+            } else {
+                // if no explicit locale has been set on this request, use one from the session
+	        $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
+            }
         }
     }
 
