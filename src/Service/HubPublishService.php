6aaa03bea01dc3b5b70b07518a8d0e0a3fa4b3ce
Stephane MEAUDRE <smeaudre@kaliop.com>
2024-01-05 14:21:52 +0100

    Create Mercure publish service

---
+++ src/Service/HubPublishService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mercure\HubInterface;
+use Symfony\Component\Mercure\Update;
+
+
+class HubPublishService
+{
+
+	public function __construct(
+		protected ?string $mercureUrl;
+		private HubInterface $hub
+	) { }
+	
+
+	public function publish(
+		string $message
+	): void
+	{
+		$update = new Update(
+			$this->mercureUrl,
+			json_encode(['message' => $message, 'date' => new DateTime('now'))
+		);
+		
+		try { 
+			$this->hub->publish($update);
+		} catch (\Exception $e) { }
+		
+	}
+	
+}
