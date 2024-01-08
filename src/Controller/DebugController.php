3466e6197484c2a7a282096a940e350f4345161d
Stephane MEAUDRE <smeaudre@kaliop.com>
2024-01-08 08:43:43 +0100

    debug

--- src/Controller/DebugController.php
+++ src/Controller/DebugController.php
@@ -34,7 +34,7 @@ class DebugController extends AbstractController
     	try {
     	    $publish = $notificationService->send('coucou');
     	} catch (\Exception $e) {
-    	    echo '<pre>Error: '; print_r($e);
+    	    echo '<pre>Error: '; var_dump($e);
     	}
     	
 	return new Response($publish);
