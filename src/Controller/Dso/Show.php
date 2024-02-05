5614e1bbe748ba8a2f1936b2632930d52e182905
Stephane MEAUDRE <smeaudre@kaliop.com>
2024-02-05 15:35:05 +0100

    Add redirection for dso

--- src/Controller/Dso/Show.php
+++ src/Controller/Dso/Show.php
@@ -53,65 +53,67 @@ class Show extends AbstractController
         $separator = trim(Utils::URL_CONCAT_GLUE);
         [$idDso, ] = explode($separator, $id);
 
-        try {
-            /** @var DsoDTO $dso */
-            $params['dso'] = $dso = $dsoManager->getDso($idDso);
+        return $this->redirect(sprintf('https://www.astro-otter.space/catalog/%s', $idDso));
 
-        } catch (NotFoundException $e) {
-            throw new NotFoundHttpException();
-        }
-
-        $constellation = $dso->getConstellation();
-        $params['dsoData'] = $dsoManager->formatVueData($dso);
-
-        // Image cover
-        $params['imgCoverAlt'] = ($dso->getAstrobin()->title) ? sprintf('"%s" by %s', $dso->getAstrobin()->title, $dso->getAstrobin()->user) : null;
-
-        // List of Dso from same constellation
-        $listDso = $dsoManager->getListDsoFromConst($dso->getConstellation()->getId(), $id, 0, 20);
-
-        $params['dso_by_const'] = $dsoDataTransformer->listVignettesView($listDso);
-        $params['list_types_filters'] = $this->buildFiltersWithAll($listDso) ?? [];
-
-        // Map
-        $params['geojson_dso'] = [
-            "type" => "FeatureCollection",
-            "features" => [$dso->geoJson()]
-        ];
-        $params['geojson_center'] = $dso->getGeometry()['coordinates'];
-
-        // List images
-        $images = $astrobinService->listImagesBy($dso->getId());
-        if (!is_null($images)) {
-            if (1 < $images->count) {
-                $listImages = array_map(static function(Image $image) {
-                    return $image->url_regular;
-                }, iterator_to_array($images));
-            } elseif(1 === $images->count) {
-                $listImages = [$images->getIterator()->current()->url_regular];
-            }
-        }
-
-        $params['images'] = $listImages ?? null; //array_filter($listImages);
-        $params['breadcrumbs'] = $this->buildBreadcrumbs($dso, $this->router, $dso->title());
-
-        $response = $this->render('pages/dso.html.twig', $params);
-
-        // cache expiration
-        $response->setPublic();
-        $response->setSharedMaxAge(self::HTTP_TTL);
-        $response->headers->addCacheControlDirective('must-revalidate', true);
-
-        // cache validation
-        $response->setLastModified($dso->getUpdatedAt());
-
-        $listDsoIdHeaders = [
-            md5(sprintf('%s_%s', $id, $request->getLocale())),
-            md5(sprintf('%s_cover', $id))
-        ];
-        $response->headers->set('x-dso-id', implode(' ', $listDsoIdHeaders));
-
-        return $response;
+//        try {
+//            /** @var DsoDTO $dso */
+//            $params['dso'] = $dso = $dsoManager->getDso($idDso);
+//
+//        } catch (NotFoundException $e) {
+//            throw new NotFoundHttpException();
+//        }
+//
+//        $constellation = $dso->getConstellation();
+//        $params['dsoData'] = $dsoManager->formatVueData($dso);
+//
+//        // Image cover
+//        $params['imgCoverAlt'] = ($dso->getAstrobin()->title) ? sprintf('"%s" by %s', $dso->getAstrobin()->title, $dso->getAstrobin()->user) : null;
+//
+//        // List of Dso from same constellation
+//        $listDso = $dsoManager->getListDsoFromConst($dso->getConstellation()->getId(), $id, 0, 20);
+//
+//        $params['dso_by_const'] = $dsoDataTransformer->listVignettesView($listDso);
+//        $params['list_types_filters'] = $this->buildFiltersWithAll($listDso) ?? [];
+//
+//        // Map
+//        $params['geojson_dso'] = [
+//            "type" => "FeatureCollection",
+//            "features" => [$dso->geoJson()]
+//        ];
+//        $params['geojson_center'] = $dso->getGeometry()['coordinates'];
+//
+//        // List images
+//        $images = $astrobinService->listImagesBy($dso->getId());
+//        if (!is_null($images)) {
+//            if (1 < $images->count) {
+//                $listImages = array_map(static function(Image $image) {
+//                    return $image->url_regular;
+//                }, iterator_to_array($images));
+//            } elseif(1 === $images->count) {
+//                $listImages = [$images->getIterator()->current()->url_regular];
+//            }
+//        }
+//
+//        $params['images'] = $listImages ?? null; //array_filter($listImages);
+//        $params['breadcrumbs'] = $this->buildBreadcrumbs($dso, $this->router, $dso->title());
+//
+//        $response = $this->render('pages/dso.html.twig', $params);
+//
+//        // cache expiration
+//        $response->setPublic();
+//        $response->setSharedMaxAge(self::HTTP_TTL);
+//        $response->headers->addCacheControlDirective('must-revalidate', true);
+//
+//        // cache validation
+//        $response->setLastModified($dso->getUpdatedAt());
+//
+//        $listDsoIdHeaders = [
+//            md5(sprintf('%s_%s', $id, $request->getLocale())),
+//            md5(sprintf('%s_cover', $id))
+//        ];
+//        $response->headers->set('x-dso-id', implode(' ', $listDsoIdHeaders));
+//
+//        return $response;
     }
 
 }
